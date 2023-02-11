5aad1e6272a01b638a2d909896cc76dd0e341cbf
prayogatriady <prayogatriady.work@gmail.com>
2023-02-11 22:56:20 +0700

    Add .env file and adapt the code

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,5 @@
-go get -u "gorm.io/gorm" "gorm.io/driver/mysql" "github.com/gin-gonic/gin" "github.com/dgrijalva/jwt-go"
+go get -u "gorm.io/gorm"
+go get -u "gorm.io/driver/mysql"
+go get -u "github.com/gin-gonic/gin"
+go get -u "github.com/dgrijalva/jwt-go"
+go get -u "github.com/joho/godotenv"
--- utils/db.go
+++ utils/db.go
@@ -2,19 +2,29 @@ package utils
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/prayogatriady/twitter-like/entities"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func InitDB() (*gorm.DB, error) {
-	USERNAME := "root"
-	PASSWORD := "root"
-	PORT := 3306
-	DBNAME := "twitter_like"
-	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		USERNAME, PASSWORD, PORT, DBNAME)
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	DbHost := os.Getenv("DB_HOST")
+	DbUser := os.Getenv("DB_USER")
+	DbPassword := os.Getenv("DB_PASSWORD")
+	DbName := os.Getenv("DB_NAME")
+	DbPort := os.Getenv("DB_PORT")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", DbUser, DbPassword, DbHost, DbPort, DbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
